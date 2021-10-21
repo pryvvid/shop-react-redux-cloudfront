f8a85b32568a187563fb736f6ecd8bd4e150f97c
pryvvid <pryvvid@gmail.com>
2021-10-21 15:20:39 +0300

    fix: cart api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://luai2aoc1i.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://fmfiacgetl.execute-api.eu-west-1.amazonaws.com",
-  cart: "http://pryvvid-cart-app-develop.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://nn9tlrdcij.execute-api.eu-west-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
